a439b99091a631ff48cf5db38b387a01d52fd96e
Tuhin S Chetia <54241736+tuhin-shikhar@users.noreply.github.com>
2019-09-08 13:34:52 +0530

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -1,20 +1,26 @@
 public class Program4
 {
-    public boolean isitPalindrome( String s )
-    {       
-        if ( s.length() % 2 != 0 )
+    private static long reversed(long n)
+    {
+        long reverse = 0;
+        long last;
+        while(n>0)
         {
-            s = s.substring( 0, s.length() / 2 ) + s.substring( (s.length() / 2) + 1);
+            last=n%10;
+            reverse=(reverse*10)+last;
+            n=n/10;
         }
-
-        return new StringBuffer( s.substring( 0, s.length() / 2) ).
-                                 reverse().toString().equals( s.substring( s.length() / 2) );
+        return reverse;
+    }
+    
+    private static boolean isitPalindrome(long n )
+    {       
+        return reversed(n) == n;
     }
 
 
     public static void main(String[] args)
     {
-        Program4 e = new Program4();
         long bigboi = 0;
 
         int a1 = 999, b1 = 999; // doing it from 999 is easier to get the highest palindrome number.
@@ -22,7 +28,7 @@ public class Program4
             {
                 for (int a2 = a1; a2 > 0; a2--)
                 {
-                    if ( e.isitPalindrome( new Long(a2*b2).toString() ) )
+                    if ( isitPalindrome(a2*b2))
                     {
                         if ( a2*b2 > bigboi )
                         {
