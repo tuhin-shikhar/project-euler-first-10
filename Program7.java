b933585f4186e203c833358ac7e2b8da923a306d
Tuhin S Chetia <54241736+tuhin-shikhar@users.noreply.github.com>
2019-09-09 14:30:07 +0530

    Update Program7.java

--- Program7.java
+++ Program7.java
@@ -3,30 +3,34 @@ public class Problem7
 
     
     /* returns the nth prime number */
-    public static long nthPrime(long n) 
+    public static long main() 
     {
         int numberOfPrimes = 0;
         long prime = 1;
         
-        while (numberOfPrimes < n) {
+        while (numberOfPrimes < 10001) 
+        {
             prime++;
-            if (IsItPrimeTho(prime)) {
+            if (IsItPrimeTho(prime)) 
+            {
                 numberOfPrimes++;
             }
         }
         return prime;
     }
     
-    /* returns true if parameter n is a prime number, false if composite or neither */
-    public static boolean IsItPrimeTho(long n) 
-    {
+
+    private static boolean IsItPrimeTho(long n) 
+    {   
         if (n < 2) 
         return false;
         else if (n == 2) 
         return true;
         for (int i = 2; i < Math.pow(n, 0.5) + 1; i++)
-            if (n % i == 0)
-                return false;
-        return true;
+           { 
+               if (n % i == 0)
+                return false;                                
+           }
+       return true;
     }
 }
