21fb1aaa666db5e7524e47f9f955bcddc31e3814
Tuhin S Chetia <54241736+tuhin-shikhar@users.noreply.github.com>
2019-09-09 14:32:05 +0530

    Update Program10.java

--- Program10.java
+++ Program10.java
@@ -1,16 +1,18 @@
-public class Problem10
+public class Problem10ver2
  {
 
     /* returns true if parameter n is a prime number,*/
-    public static boolean isOptimusPrime(long n) 
+    private static boolean isOptimusPrime(long n) 
     {
-        if (n < 2) 
-        return false;
-        else if (n == 2) 
-        return true;
-        for (int i = 2; i < Math.pow(n, 0.5) + 1; i++)
-            if (n % i == 0)
-                return false;
+        int counter=0;
+        long sqroot=(long)Math.sqrt(n);
+        for (int x=1;x<=sqroot;x++)
+        {
+         if(n%x==0)
+         counter++;
+         else if(counter>1)
+         return false;
+        }
         return true;
     }
     
